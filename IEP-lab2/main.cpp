92657048523e3b6fc5a6010a26eb80094cd199e2
EduardBabuscov <babuscoveduard@gmail.com>
2017-10-21 15:27:33 +0300

    Added pin in main

--- IEP-lab2/main.cpp
+++ IEP-lab2/main.cpp
@@ -17,28 +17,30 @@
 // $Id: RF22.h,v 1.21 2012/05/30 01:51:25 mikem Exp $
 #include "bcm2835.h"
 #include <stdio.h>
+#include <pin.hpp>
 // Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
 #define PIN RPI_GPIO_P1_11
 int main(int argc, char **argv)
 {
+    Pin *pin=new Pin(RPI_GPIO_P1_11,BCM2835_GPIO_FSEL_OUTP);
     // If you call this, it will not actually access the GPIO
     // Use for testing
     bcm2835_set_debug(1);
     if (!bcm2835_init())
       return 1;
     // Set the pin to be an output
-    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+   //bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
     // Blink
     while (1)
     {
         // Turn it on
-        bcm2835_gpio_write(PIN, HIGH);
+        pin->set(HIGH);
 
         // wait a bit
         bcm2835_delay(500);
 
         // turn it off
-        bcm2835_gpio_write(PIN, LOW);
+        pin->set(LOW);;
 
         // wait a bit
         bcm2835_delay(500);\
