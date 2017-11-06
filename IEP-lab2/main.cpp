b4bfcea09ecae3caf2e190795eb2ad69ea2d02c1
Leopold <vladradulescu.vr@gmail.com>
2017-11-06 16:25:18 +0200

    Update main.cpp

--- IEP-lab2/main.cpp
+++ IEP-lab2/main.cpp
@@ -16,42 +16,38 @@
 // Copyright (C) 2011 Mike McCauley
 // $Id: RF22.h,v 1.21 2012/05/30 01:51:25 mikem Exp $
 #include "bcm2835.h"
+
+#include "pin.hpp"
+#include "pwm.hpp"
+
 #include <stdio.h>
-#include <pin.hpp>
-#include <pwm.hpp>
 // Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
 #define PIN RPI_GPIO_P1_11
 int main(int argc, char **argv)
 {
-    Pin *pin=new Pin(RPI_GPIO_P1_11,BCM2835_GPIO_FSEL_OUTP);
+    Pin myPin(0, INPUT, PULL_UP);
+    Pwm myPwm(100, 50);
     // If you call this, it will not actually access the GPIO
     // Use for testing
-  // Pwm *pwm=new Pwm(1024,0);
-    //int fill=0;
     bcm2835_set_debug(1);
     if (!bcm2835_init())
       return 1;
     // Set the pin to be an output
-   bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
     // Blink
     while (1)
     {
-        //fill+=10;
-        // Turn it on
-        pin->set(HIGH);
-      // pwm->setFillFactor(fill);
-        // wait a bit
-        bcm2835_delay(500);
-
-        // turn it off
-        pin->set(LOW);;
-
-        // wait a bit
-        bcm2835_delay(500);
-       //bcm2835_delay(1);
+        // Consideram butonul apasat la 0 logic
+        if (myPin.get() == 0) {
+            if (myPwm.getFillFactor() == 100) {
+                myPwm.setFillFactor(0);
+            }
+            else {
+                myPwm.setFillFactor(myPwm.getFillFactor() + 10);
+            }
+        }
     }
     bcm2835_close();
-   // pwm->~Pwm();
-    pin->~Pin();
     return 0;
 }
+
